14dac1a8d800d12bc79eb8bbeedcdb65b2ea06a6
Kr36pton <jmmesa37@misena.edu.co>
2022-05-27 00:25:04 -0500

    Parte 09 Solucionada

--- src/main/java/io/pivotal/literx/Part09Adapt.java
+++ src/main/java/io/pivotal/literx/Part09Adapt.java
@@ -19,6 +19,7 @@ package io.pivotal.literx;
 import java.util.concurrent.CompletableFuture;
 
 import io.pivotal.literx.domain.User;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
@@ -37,54 +38,54 @@ import reactor.core.publisher.Mono;
  *
  * @author Sebastien Deleuze
  */
-public class Part09Adapt {
+public class Part09Adapt{
 
 //========================================================================================
 
 	// TODO Adapt Flux to RxJava Flowable
-	Flowable<User> fromFluxToFlowable(Flux<User> flux) {
-		return null;
+	Flowable<User> fromFluxToFlowable(Flux<User> flux){
+		return Flowable.fromPublisher(flux); 
 	}
 
 	// TODO Adapt RxJava Flowable to Flux
-	Flux<User> fromFlowableToFlux(Flowable<User> flowable) {
-		return null;
+	Flux<User> fromFlowableToFlux(Flowable<User> flowable){
+		return Flux.from(flowable); 
 	}
 
 //========================================================================================
 
 	// TODO Adapt Flux to RxJava Observable
-	Observable<User> fromFluxToObservable(Flux<User> flux) {
-		return null;
+	Observable<User> fromFluxToObservable(Flux<User> flux){
+		return Observable.fromPublisher(flux); 
 	}
 
 	// TODO Adapt RxJava Observable to Flux
-	Flux<User> fromObservableToFlux(Observable<User> observable) {
-		return null;
+	Flux<User> fromObservableToFlux(Observable<User> observable){
+		return Flux.from(observable.toFlowable(BackpressureStrategy.BUFFER)); 
 	}
 
 //========================================================================================
 
 	// TODO Adapt Mono to RxJava Single
-	Single<User> fromMonoToSingle(Mono<User> mono) {
-		return null;
+	Single<User> fromMonoToSingle(Mono<User> mono){
+		return Single.fromPublisher(mono); 
 	}
 
 	// TODO Adapt RxJava Single to Mono
-	Mono<User> fromSingleToMono(Single<User> single) {
-		return null;
+	Mono<User> fromSingleToMono(Single<User> single){
+		return Mono.from(single.toFlowable()); 
 	}
 
 //========================================================================================
 
 	// TODO Adapt Mono to Java 8+ CompletableFuture
-	CompletableFuture<User> fromMonoToCompletableFuture(Mono<User> mono) {
-		return null;
+	CompletableFuture<User> fromMonoToCompletableFuture(Mono<User> mono){
+		return mono.toFuture(); 
 	}
 
 	// TODO Adapt Java 8+ CompletableFuture to Mono
-	Mono<User> fromCompletableFutureToMono(CompletableFuture<User> future) {
-		return null;
+	Mono<User> fromCompletableFutureToMono(CompletableFuture<User> future){
+		return Mono.fromFuture(future); 
 	}
 
 }
